3fc1756a7a6827a9f5203f9557546a7d6434a670
Cameron Custer <73217097+cameroncuster@users.noreply.github.com>
2024-03-25 15:14:04 -0400

    Update path.ts

--- scripts/src/path.ts
+++ scripts/src/path.ts
@@ -1,7 +1,3 @@
 export default function buildPath(route: string) {
-  if (process.env.NODE_ENV === 'production') {
-    return 'https://us-central1-ofast-e6866.cloudfunctions.net/api' + route
-  } else {
-    return 'http://localhost:5001/ofast-e6866/us-central1/api' + route
-  }
+  return 'https://us-central1-ofast-e6866.cloudfunctions.net/api' + route
 }
