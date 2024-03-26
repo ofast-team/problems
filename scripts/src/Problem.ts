393fe84e5dc93fa502f76f5ef0852b00a262fea4
Cameron Custer <ccuster@rapidcity.ptl.ucf.edu>
2024-03-26 13:35:42 -0400

    update the problem comp to have TL, ML, author, src

--- scripts/src/Problem.ts
+++ scripts/src/Problem.ts
@@ -1,6 +1,10 @@
 export type Problem = {
   problemID: string
   title: string
+  timeLimit: number
+  memoryLimit: number
+  souce: string
+  author: string
   text: string
   problem: string
   input: string
