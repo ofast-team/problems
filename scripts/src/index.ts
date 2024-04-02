04dd8f29a677f9d23345eed84b94f43696d74955
Cameron Custer <ccuster@rapidcity.ptl.ucf.edu>
2024-04-01 23:09:29 -0400

    then/catch

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -8,4 +8,17 @@ import { updateProblems } from './updateProblems'
 import { updateProblemData } from './updateProblemData'
 
 updateProblems(problems)
+  .then(() => {
+    console.log('Problems updated')
+  })
+  .catch((error) => {
+    console.error('Failed to update problems:', error)
+  })
+
 updateProblemData(problemData)
+  .then(() => {
+    console.log('Problem data updated')
+  })
+  .catch((error) => {
+    console.error('Failed to update problem data:', error)
+  })
